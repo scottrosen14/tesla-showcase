79d2f7da43345cfe4f80883639051fdd5460e6b1
scottrosen14 <scott.rosen14@gmail.com>
2022-08-14 22:58:40 -0700

    Switch color palette to color picker

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import state from '../state'
+import { useState } from 'react';
+import { HexColorPicker} from 'react-colorful';
 
 const sharedStyles = {
     height: 50,
@@ -8,68 +10,32 @@ const sharedStyles = {
     cursor: 'pointer'
 };
 
-const ColorPicker = props => {
-    const handleClick = e => {
-        if (!state.activeMesh) return;
-        state.activeMesh.material.color = new THREE.Color(e.target.style.background)
+const ColorPickerContainer = props => {
+    const [color, setColor] = useState('#178BE3');
+    if (state.activeMesh && state.activeMesh.material) {
+        state.activeMesh.material.color = new THREE.Color(color);
     }
     return (
         <div
             style={{
                 position: 'absolute',
+                border: "1px solid orange",
                 zIndex: 1,
                 left: 0,
                 right: 0,
                 margin: 'auto',
                 width: 'fit-content',
                 display: 'flex',
+                justifySelf: 'end',
                 top: '20px'
             }}
         >
-            <div
-                onClick={handleClick}
-                style={{
-                    background: 'rgb(243, 246, 247)',
-                    ...sharedStyles
-                }}
-            />
-            <div
-                onClick={handleClick}
-                style={{
-                    background: 'black',
-                    ...sharedStyles
-                }}
-            />
-            <div
-                onClick={handleClick}
-                style={{
-                    background: 'red',
-                    ...sharedStyles
-                }}
-            />
-            <div
-                onClick={handleClick}
-                style={{
-                    background: 'rgb(30, 75, 93)',
-                    ...sharedStyles
-                }}
-            />
-            <div
-                onClick={handleClick}
-                style={{
-                    background: '#000d89',
-                    ...sharedStyles
-                }}
-            />
-            <div
-                onClick={handleClick}
-                style={{
-                    background: '#175421',
-                    ...sharedStyles
-                }}
-            />
+            <HexColorPicker 
+                color={color} 
+                onChange={setColor}
+            />;
         </div>
     )
 }
 
-export default ColorPicker;
+export default ColorPickerContainer;
